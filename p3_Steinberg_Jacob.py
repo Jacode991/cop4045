4b76eb6fce2a93d0ca2dc54c6c4b54e086b637bc
Jacode991 <jsteinberg2024@fau.edu>
2026-09-20 18:50:59 -0400

    Update p3_Steinberg_Jacob.py

--- p3_Steinberg_Jacob.py
+++ p3_Steinberg_Jacob.py
@@ -55,7 +55,7 @@ def get_friends(sn: dict, user1: str, distance: int) -> list:
 
       current_level = next_level
 
-      if len(current_level) == 0
+      if len(current_level) == 0:
           break
 
     return friends_found
